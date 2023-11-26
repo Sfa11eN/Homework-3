2a21a96377b0b7fb77d07894a9b303c9c00dc654
Sfa11eN <sfallenn@gmail.com>
2023-11-27 01:09:03 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,8 @@ public class Main {
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
 
-            System.out.println(  " Задача 1 "  );
+
+            System.out.println(" Задача 1 ");
 
             byte byteValue = 23;
             short shortValue = 200;
@@ -20,14 +21,54 @@ public class Main {
             long longValue = 300000000L;
             float floatValue = 2.23f;
             double doubleValue = 300000000d;
-            System.out.println("Значение переменной byteValue с типом byte равно " + byteValue );
-            System.out.println("Значение переменной shortValue с типом byte равно " + shortValue );
-            System.out.println("Значение переменной intValue с типом byte равно " + intValue );
-            System.out.println("Значение переменной longValue с типом byte равно " + longValue );
-            System.out.println("Значение переменной floatValue с типом byte равно " + floatValue );
-            System.out.println("Значение переменной doubleValue с типом byte равно " + doubleValue );
+            System.out.println("Значение переменной byteValue с типом byte равно " + byteValue);
+            System.out.println("Значение переменной shortValue с типом byte равно " + shortValue);
+            System.out.println("Значение переменной intValue с типом byte равно " + intValue);
+            System.out.println("Значение переменной longValue с типом byte равно " + longValue);
+            System.out.println("Значение переменной floatValue с типом byte равно " + floatValue);
+            System.out.println("Значение переменной doubleValue с типом byte равно " + doubleValue);
 
-            System.out.println(" Задача 2 " );
+        }
+        {
+            System.out.println("  Задача 2  ");
+
+            float floatValue = 27.12f;
+            long longValue = 987678965549L;
+            float floatValue2 = 2.786f;
+            int intValue = 569;
+            short shortValue = -159;
+            int intValue2 = 27897;
+            byte byteValue2 = 67;
+
+            System.out.println("Значение переменной floatValue с типом float равно " + floatValue);
+            System.out.println("Значение переменной longValue с типом long равно " + longValue);
+            System.out.println("Значение переменной floatValue2 с типом float равно " + floatValue2);
+            System.out.println("Значение переменной intValue с типом int равно " + intValue);
+            System.out.println("Значение переменной shortValue с типом short равно " + shortValue);
+            System.out.println("Значение переменной intValue2 с типом int равно " + intValue2);
+            System.out.println("Значение переменной byteValue2 с типом byte равно " + byteValue2);
+
+        }
+        {
+            System.out.println("  Задача 3  ");
+
+            byte firstClass = 23;
+            byte secondClass = 27;
+            byte thirdClass = 30;
+            short allPaper = 480;
+
+            System.out.println(" В классе Людмилы Павловны " + firstClass + " ученика," + " в классе Анны Сергеевны " + secondClass + " учеников, а" + " в классе Екатерины Андреевны, " + thirdClass + " учеников .");
+
+            int paperForSchoolboy = allPaper / (firstClass + secondClass + thirdClass);
+            System.out.println(" Всего было выделено " + allPaper + " листов бумаги " );
+            System.out.println(" На каждого ученика рассчитано " + paperForSchoolboy + " листов бумаги ");
+
+        }
+        {
+            System.out.println("  Задача 4  ");
+
+
+
 
         }
     }
