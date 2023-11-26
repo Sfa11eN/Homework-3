5428e3a05e784d3a40c67a3e86f366db3bf0b5d5
Sfa11eN <sfallenn@gmail.com>
2023-11-26 23:28:51 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,28 @@ public class Main {
         System.out.println("Hello and welcome!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        {
 
             // Press Shift+F9 to start debugging your code. We have set one breakpoint
             // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+
+            System.out.println(  " Задача 1 "  );
+
+            byte byteValue = 23;
+            short shortValue = 200;
+            int intValue = 700000;
+            long longValue = 300000000L;
+            float floatValue = 2.23f;
+            double doubleValue = 300000000d;
+            System.out.println("Значение переменной byteValue с типом byte равно " + byteValue );
+            System.out.println("Значение переменной shortValue с типом byte равно " + shortValue );
+            System.out.println("Значение переменной intValue с типом byte равно " + intValue );
+            System.out.println("Значение переменной longValue с типом byte равно " + longValue );
+            System.out.println("Значение переменной floatValue с типом byte равно " + floatValue );
+            System.out.println("Значение переменной doubleValue с типом byte равно " + doubleValue );
+
+            System.out.println(" Задача 2 " );
+
         }
     }
 }
